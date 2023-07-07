053e08371fcd378d2b97447f2b7381877e254c0e
Oliver <oliver.lewandowski1@gmail.com>
2023-07-07 19:41:25 +0200

    theme switcher, overall improvements

--- components/components/nav/navbar.tsx
+++ components/components/nav/navbar.tsx
@@ -3,6 +3,7 @@ import MainNav from "@/components/components/nav/main-nav";
 import StoreSwitcher from "@/components/components/store-switcher/store-switcher";
 import { checkUserAuth } from "@/lib/auth-utils";
 import prismadb from "@/lib/prisma-db";
+import { ModeToggle } from "@/components/components/mode-toogle/mode-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -21,6 +22,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className='mx-6' />
         <div className='flex items-center ml-auto space-x-4'>
+          <ModeToggle />
           <UserButton afterSignOutUrl='/' />
         </div>
       </div>
